2dcf4fb64c7efd2a4a099ed032046a85869b2646
xboy <muhammadalizufan@gmail.com>
2024-03-06 22:06:19 +0700

    fix: get detail query

--- api/v1/books/index.ts
+++ api/v1/books/index.ts
@@ -14,7 +14,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         switch (req.method?.toUpperCase()) {
             case "GET": {
                 if (id > 0) {
-                    const book = await prisma.book.findUnique({
+                    const book = await prisma.book.findFirst({
                         where: { id }
                     })
                     return res.status(200).json(book)
